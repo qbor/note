e8c9860a614b2b171f0afa4f62e085c1b44ca3ea
qbor <qumuqubu@qq.com>
2026-06-28 22:02:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.__SUPABASE_CONFIG__ = {
-    // 这里改为走当前站点的代理路径，避免前端直接暴露 Supabase 最终域名
     url: window.location.origin + '/supabase-api',
     key: 'sb_publishable_5YdNr0DOSwAGpGKhvz0V_Q_6X_G8Qc7'
 };
